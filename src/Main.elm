41bf604878c9091135f46cac1fade8c5986a2fe8
Joël Quenneville <joelq@thoughtbot.com>
2018-09-20 21:04:50 -0400

    Convert static page to to `Browser.document`

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 
 
-main : Html a
+type alias Model =
+    String
+
+
+type Msg
+    = Noop
+
+
+main : Program {} Model Msg
 main =
-    text "muses"
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+init : {} -> ( Model, Cmd Msg )
+init _ =
+    ( "", Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Muses"
+    , body = [ text "Muses" ]
+    }
