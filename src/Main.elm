aa2c2e23e98a0258e75c0b16e1239d17a96e4836
Joël Quenneville <joelq@thoughtbot.com>
2018-09-20 21:19:33 -0400

    Allow users to select a deck
    Use fake decks for now

--- src/Main.elm
+++ src/Main.elm
@@ -2,14 +2,30 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+type alias GameState =
+    { selectedDeck : Deck }
+
+
+type Game
+    = Choosing
+    | Playing GameState
+    | Complete
 
 
 type alias Model =
-    String
+    Game
 
 
 type Msg
-    = Noop
+    = SelectDeck Deck
+
+
+type Deck
+    = Flashy
+    | SlowAndSteady
 
 
 main : Program {} Model Msg
@@ -24,16 +40,56 @@ main =
 
 init : {} -> ( Model, Cmd Msg )
 init _ =
-    ( "", Cmd.none )
+    ( Choosing, Cmd.none )
+
+
+deckChoices : List Deck
+deckChoices =
+    [ Flashy, SlowAndSteady ]
+
+
+deckName : Deck -> String
+deckName deck =
+    case deck of
+        SlowAndSteady ->
+            "Slow and Steady"
+
+        Flashy ->
+            "Flashy"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SelectDeck deck ->
+            ( Playing <| GameState deck, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Muses"
-    , body = [ text "Muses" ]
+    , body = [ h1 [] [ text "Muses" ], viewGame model ]
     }
+
+
+viewGame : Game -> Html Msg
+viewGame game =
+    case game of
+        Choosing ->
+            choices
+
+        Playing { selectedDeck } ->
+            text <| "Playing with: " ++ deckName selectedDeck
+
+        Complete ->
+            text "Someone won!"
+
+
+choices : Html Msg
+choices =
+    ul [] <| List.map choice deckChoices
+
+
+choice : Deck -> Html Msg
+choice deck =
+    li [ onClick <| SelectDeck deck ] [ text <| deckName deck ]
